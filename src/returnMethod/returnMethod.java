0f5a8894ed8e6a346276615837ea4316c49f5985
alexcharlesthain <alexcharlesthain@gmail.com>
2018-11-20 10:53:34 +0000

    first commit

---
+++ src/returnMethod/returnMethod.java
@@ -0,0 +1,14 @@
+package returnMethod;
+
+public class returnMethod {
+
+	public static void main(String[] args) {	
+			sayHelloWorld();
+			}
+				
+				
+		public static void sayHelloWorld() {
+			System.out.println("Hello World!");
+
+}
+}
---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Admin
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		String helloWorldString = "Hello World";
+		System.out.println(helloWorldString);
+	}
+}
