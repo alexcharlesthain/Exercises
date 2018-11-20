d4530f9fefd6c21efd6da968dcc4d88c0e0b5513
alexcharlesthain <45198025+alexcharlesthain@users.noreply.github.com>
2018-11-20 13:46:14 +0000

    Add files via upload

---
+++ src/ParametersOperators.java
@@ -0,0 +1,29 @@
+package parametersOperators;
+
+public class ParametersOperators {
+	public static void main(String[] args)
+	{
+		int a = 40;
+		int b = 40;
+		
+		
+		System.out.println("The sum is = " + multiply(40, 40, false));
+		
+		
+		
+	}
+	
+	public static int multiply(int a, int b, boolean isMultiply) {
+		if (isMultiply) {
+			return a * b;
+		
+		}
+		
+		return a + b;
+		 		
+	}
+}
+
+
+
+
